0e8e8caf5aa8b0f5745a6574bd4a68371dba69a6
Karin <karin.stenwall@gmail.com>
2021-11-18 10:32:29 +0100

    feat: move styling to module

--- pages/present.tsx
+++ pages/present.tsx
@@ -1,23 +1,21 @@
 import type { NextPage } from 'next';
+import s from '../styles/Present.module.scss';
 
 const Present: NextPage = () => {
   return (
     <main className="main">
       <h3>Hover the box</h3>
-      <div className="present-wrapper">
-        {/* <img className="img" src="https://via.placeholder.com/150" /> */}
-        <div className="box">
-          <div className="box-wrapper">
-            <img className="img" src="https://via.placeholder.com/150" />
-            <div className="box-body">
-              {/* <img className="img" src="https://via.placeholder.com/150" /> */}
-              <div className="box-lid">
-                <div className="box-bowtie"></div>
+      <div className={s['present-wrapper']}>
+        <div className={s.box}>
+          <div className={s['box-wrapper']}>
+            <img className={s.img} src="https://via.placeholder.com/150" />
+            <div className={s['box-body']}>
+              <div className={s['box-lid']}>
+                <div className={s['box-bow']}></div>
               </div>
             </div>
           </div>
         </div>
-        {/* <img className="img" src="https://via.placeholder.com/150" /> */}
       </div>
     </main>
   );
