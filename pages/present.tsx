4d88f641e2204c6d9e562435ac5d4f86476ee3cc
Karin <karin.stenwall@gmail.com>
2021-11-18 14:56:04 +0100

    feat: box animation + event handler

--- pages/present.tsx
+++ pages/present.tsx
@@ -1,14 +1,25 @@
 import type { NextPage } from 'next';
+import { useState } from 'react';
 import s from '../styles/Present.module.scss';
 
 const Present: NextPage = () => {
+  const [open, setOpen] = useState(false);
+
+  const openBox = () => {
+    setOpen(true);
+  }
+
   return (
     <main className="main">
       <h3>Hover the box</h3>
       <div className={s['present-wrapper']}>
         <div className={s.box}>
-          <div className={s['box-wrapper']}>
-            <img className={s.img} src="https://via.placeholder.com/150" />
+          <div className={open ? [s.open, s['box-wrapper']].join(' ') : s['box-wrapper']} onClick={openBox}>
+            <div className={s.glow} />
+            <img
+              className={[s.img, s.star].join(' ')}
+              src="https://via.placeholder.com/150"
+            />
             <div className={s['box-body']}>
               <div className={s['box-lid']}>
                 <div className={s['box-bow']}></div>
