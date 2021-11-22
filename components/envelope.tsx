eb9a506e361f223e1c3ec5bd261cd1e29673ba70
Karin <karin.stenwall@gmail.com>
2021-11-22 11:03:24 +0100

    feat: rules title + animation

--- components/envelope.tsx
+++ components/envelope.tsx
@@ -1,16 +1,107 @@
 import { gsap } from 'gsap';
 import { CSSRulePlugin } from 'gsap/dist/CSSRulePlugin';
+import { MotionPathPlugin } from 'gsap/dist/MotionPathPlugin';
 import Rules from './rules';
 
 gsap.registerPlugin(CSSRulePlugin);
+gsap.registerPlugin(MotionPathPlugin);
 
 interface Props {}
 
 const Envelope = (props: Props) => {
   let t1 = gsap.timeline({ paused: true });
-  let t2 = gsap.timeline({ paused: true });
+  let t2 = gsap.timeline();
   let flap = CSSRulePlugin.getRule('.envelope:before');
   let letter = CSSRulePlugin.getRule('.letter');
+  let ruleText = CSSRulePlugin.getRule('.rules-title');
+
+  // t2.from(ruleText, {
+  //   cssRule: {
+  //     yPercent: -2000,
+  //   }
+  // })
+  // .to(ruleText, {
+  //   duration: 0.5,
+  //   ease: 'sine.in',
+  //   cssRule: {
+  //     yPercent: -300,
+  //   }
+  // })
+  // .to(ruleText, {
+  //   duration: 0.5,
+  //   ease: 'sine.in',
+  //   cssRule: {
+  //     translateY: 27,
+  //   }
+  // })
+  // .to(ruleText, {
+  //   duration: 0.5,
+  //   ease: 'sine.in',
+  //   cssRule: {
+  //     translateY: -12,
+  //   }
+  // })
+  // .to(ruleText, {
+  //   duration: 0.5,
+  //   ease: 'sine.in',
+  //   cssRule: {
+  //     translateY: 7,
+  //   }
+  // })
+  // .to(ruleText, {
+  //   ease: 'bounce.out',
+  //   cssRule: {
+  //     yPercent: 0,
+  //   }
+  // });
+
+  t2.from(ruleText, {
+    cssRule: {
+      yPercent: -2000,
+    },
+  })
+  .to(ruleText, {
+    duration: 0.5,
+    ease: 'sine.in',
+    motionPath: {
+      path: [
+        { x: 0, y: 0.9 },
+        { x: 0, y: 0.03 },
+        { x: 0, y: 0.69 },
+        { x: 0, y: 0.22 }
+      ],
+      type: 'cubic'
+    }
+  })
+  .to(ruleText, {
+    duration: 2,
+    ease: 'bounce.out',
+    cssRule: {
+      yPercent: 0,
+    }
+  });
+
+  // t2.fromTo(ruleText, {
+  //   duration: 10,
+  //   ease: 'sine.in',
+  //   cssRule: {
+  //     yPercent: -2000,
+  //   },
+  //   motionPath: {
+  //     path: [
+  //       { x: 0, y: 0.9 },
+  //       { x: 0, y: 0.03 },
+  //       { x: 0, y: 0.69 },
+  //       { x: 0, y: 0.22 }
+  //     ],
+  //     type: 'cubic'
+  //   }
+  // }, {
+  //   ease: 'bounce.out',
+  //   cssRule: {
+  //     yPercent: 0,
+  //   }
+  // });
 
   t1.to(flap, {
     duration: 0.5,
@@ -99,6 +190,9 @@ const Envelope = (props: Props) => {
   return (
     <>
       <div className="content">
+        <div className="rules-title">
+          regler <span>&#10168;</span>
+        </div>
         <div className="envelope" onClick={openCard}></div>
         <div className="letter" onClick={closeCard}>
           <Rules onClose={closeCard} />
